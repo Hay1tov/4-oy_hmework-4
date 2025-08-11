62df0f7dc8efe908b39c844422fe866961ba538e
Hay1tov <eldorh35@gmail.com>
2025-08-11 13:21:27 +0500

    update

---
+++ index.js
@@ -0,0 +1,60 @@
+//                                      No_1
+
+// let n = 5  //  1 <= n <= 26  oralig'ida son kiritiladi
+// let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// let result = alphabet.slice(0, n);
+// console.log(result);
+
+
+//                                      No_2
+
+
+// function belgiTuri(symbol) {
+//   if (symbol >= "0" && symbol <= "9") {
+//     return "digit";
+//   } else if (
+//     (symbol >= "A" && symbol <= "Z") ||
+//     (symbol >= "a" && symbol <= "z")
+//   ) {
+//     return "lotin";
+//   } else {
+//     return 0;
+//   }
+// }
+
+// console.log(belgiTuri("@"));
+
+
+//                                      No_3
+
+
+
+
+
+//                                      No_4
+//                                      No_5
+//                                      No_6
+//                                      No_7
+//                                      No_8
+//                                      No_9
+//                                      No_10
+//                                      No_11
+//                                      No_12
+//                                      No_13
+//                                      No_14
+//                                      No_15
+//                                      No_16
+//                                      No_17
+//                                      No_18
+//                                      No_19
+//                                      No_20
+//                                      No_21
+//                                      No_22
+//                                      No_23
+//                                      No_24
+//                                      No_25
+//                                      No_26
+//                                      No_27
+//                                      No_28
+//                                      No_29
+//                                      No_30
